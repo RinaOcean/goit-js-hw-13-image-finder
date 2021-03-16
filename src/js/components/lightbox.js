a5e32955de4904bea02cda07942f3b525641d01a
RinaOcean <antyfifa@gmail.com>
2021-03-16 16:05:51 +0200

    cleans code

--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -4,7 +4,7 @@ import '../../../node_modules/basiclightbox/dist/basicLightbox.min.css';
 export default function setLightbox(event) {
   const bigImgUrl = event.target.dataset.lightboxImg;
   const instance = basicLightbox.create(`
-    <div style:"background-image: url('./images/loading1.gif')"><img src="${bigImgUrl}" width="800" height="600"></div>
+    <img src="${bigImgUrl}" width="800" height="600">
 `);
 
   instance.show();
